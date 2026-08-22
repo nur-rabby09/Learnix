929ab516abe439bd54542cd51cce23d7b0260c8d
nur-rabby09 <fardin.cse.00724105101037@aust.edu>
2026-08-22 14:43:28 +0600

    initial sign up page

---
+++ src/Signup.jsx
@@ -0,0 +1,83 @@
+import { useState } from 'react';
+import './SignUp.css';
+
+function SignUp() {
+  const [firstName, setFirstName] = useState('');
+  const [lastName, setLastName] = useState('');
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+  const [confirmPassword, setConfirmPassword] = useState('');
+
+  const handleSubmit = () => {
+    console.log('Sign up:', firstName, lastName, email, password, confirmPassword);
+  };
+
+  return (
+    <div className="signup-page">
+      <div className="navbar">
+        <span className="logo">Learnix</span>
+      </div>
+
+      <div className="signup-wrapper">
+        <h2 className="signup-title">Sign Up</h2>
+        <div className="signup-card">
+          <p className="signup-eyebrow">Create Account</p>
+          <div className="name-row">
+            <div className="name-field">
+              <label>First Name</label>
+              <input
+                type="text"
+                value={firstName}
+                onChange={(e) => setFirstName(e.target.value)}
+              />
+            </div>
+
+            <div className="name-field">
+              <label>Last Name</label>
+              <input
+                type="text"
+                value={lastName}
+                onChange={(e) => setLastName(e.target.value)}
+              />
+            </div>
+          </div>
+
+          <label>Email</label>
+          <input
+            type="email"
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
+          />
+
+          <label>Password</label>
+          <input
+            type="password"
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+          />
+
+          <label>Confirm Password</label>
+          <input
+            type="password"
+            value={confirmPassword}
+            onChange={(e) => setConfirmPassword(e.target.value)}
+          />
+
+          <button
+            className="signup-btn"
+            onClick={handleSubmit}
+          >
+            Sign Up
+          </button>
+
+          <p className="login-text">
+            Already Have a Account? <a href="/signup">Sign In</a>
+          </p>
+
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default SignUp;
--- src/main.jsx
+++ src/main.jsx
@@ -3,9 +3,10 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
 import Login from './login.jsx';
+import Signup from './Signup.jsx';
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <Login />
+    <Signup />
   </StrictMode>,
 )
